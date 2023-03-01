0fc9659738547fcbf677fbdeca23ca54c5b30359
Kartik Gupta <kartikgupta7267@gmail.com>
2023-03-01 21:37:34 +0530

    Update cors.js

--- routes/cors.js
+++ routes/cors.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist = ['http://localhost:3000','http://localhost:3001',"https://telegramtemperaturebot2.onrender.com/"];
+const whitelist = ['http://localhost:3000','http://localhost:3001',"https://telegramtemperaturebot2.onrender.com"];
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     if(whitelist.indexOf(req.header('Origin')) !== -1) {
